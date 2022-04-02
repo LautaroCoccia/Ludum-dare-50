b2f65dc06f61908ded09b26c0f6690d72046c6cb
Lautaro Coccia <lautaroauditore@gmail.com>
2022-04-02 18:36:33 -0300

    Fix merge

--- Assets/Scripts/Enemies/EnemyTaser.cs
+++ Assets/Scripts/Enemies/EnemyTaser.cs
@@ -22,28 +22,28 @@ public class EnemyTaser : DefaultEnemy
         {
             while (stunned == false)
             {
-                if (Vector3.Distance(Player.transform.position, transform.position) < EnemyRange)
-                {
-                    Vector3 LockOn = Player.transform.position;
-                    float DistanceToShot = 
-                    RB.velocity = Vector3.zero;
-                    yield return new WaitForSeconds(Windup);
-                    Physics.BoxCastAll();
-                    yield return new WaitForSeconds(ActiveFrames);
-
-                    yield return new WaitForSeconds(fallBack);
-                }
-                else
-                {
-                    RB.velocity = new Vector3(Player.transform.position.x - transform.position.x, 0, Player.transform.position.z - transform.position.z).normalized * MovementSpeed;
-                }
-                
-                yield return null;
+                //if (Vector3.Distance(Player.transform.position, transform.position) < EnemyRange)
+                //{
+                //    Vector3 LockOn = Player.transform.position;
+                //    float DistanceToShot = 
+                //    RB.velocity = Vector3.zero;
+                //    yield return new WaitForSeconds(Windup);
+                //    Physics.BoxCastAll();
+                //    yield return new WaitForSeconds(ActiveFrames);
+                //
+                //    yield return new WaitForSeconds(fallBack);
+                //}
+                //else
+                //{
+                //    RB.velocity = new Vector3(Player.transform.position.x - transform.position.x, 0, Player.transform.position.z - transform.position.z).normalized * MovementSpeed;
+                //}
+                //
+                //yield return null;
             }
-            else
-            {
-                yield return null;
-            }
+            //else
+            //{
+            //    yield return null;
+            //}
 
         }
         
